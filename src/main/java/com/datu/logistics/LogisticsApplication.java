4c7058465436618591bd264ff9178571631f2802
叶奕 <simon.ye@hongkongmedsourcing.com>
2021-04-23 16:14:05 +0800

    初始化工程

---
+++ src/main/java/com/datu/logistics/LogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.datu.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsApplication.class, args);
+	}
+
+}
