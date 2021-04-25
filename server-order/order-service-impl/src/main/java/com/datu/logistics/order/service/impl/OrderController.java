af28294f0db14f84a932cb7a7683a7ea05ed6012
叶奕 <simon.ye@hongkongmedsourcing.com>
2021-04-25 18:54:39 +0800

    order 项目结构优化

--- server-order/order-service-impl/src/main/java/com/datu/logistics/order/service/impl/OrderController.java
+++ server-order/order-service-impl/src/main/java/com/datu/logistics/order/service/impl/OrderController.java
@@ -1,16 +1,23 @@
 package com.datu.logistics.order.service.impl;
 
+import com.datu.logistics.order.domain.model.Order;
+import com.datu.logistics.order.domain.repository.OrderRepository;
 import com.datu.logistics.order.service.OrderApplicationService;
 import com.datu.logistics.order.service.dto.OrderDTO;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.math.BigInteger;
 
-@RestController()
+@RestController
 public class OrderController implements OrderApplicationService {
+
+    @Resource
+    private OrderRepository orderRepository;
 
     @Override
     public OrderDTO order(BigInteger orderId) {
-        return new OrderDTO(orderId);
+        Order order = orderRepository.orderOf(orderId);
+        return new OrderDTO(order.getId());
     }
 }
