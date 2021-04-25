34a3cd3fc8d343e3b119bfe30defc90079997fbc
叶奕 <simon.ye@hongkongmedsourcing.com>
2021-04-25 16:23:01 +0800

    优化配置文件、服务端口配置、pom配置

--- server-registry/src/main/java/com/datu/logistics/registry/RegistryApplication.java
+++ server-registry/src/main/java/com/datu/logistics/registry/RegistryApplication.java
@@ -1,4 +1,4 @@
-package com.datu.logistics.gateway;
+package com.datu.logistics.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
